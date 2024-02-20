c9975ed517ca092b4bc18405870bc1dc0f3d1e63
alexisquintanilla <quintanilla.alexis@gmail.com>
2024-02-20 17:33:56 -0300

    inicio

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+const myModal = document.getElementById('miModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+
+})
+
+function consulta() {
+
+    var mail = document.getElementById("emailDeuda")
+    window.open('https://www.pagoselectrogps.cl/public/areaCliente.html?' + mail.value, '_self')
+}
+
+// formulario
+
+const formularioContacto = document.getElementById('formularioContacto')
+const textNombre = document.getElementById('textNombre')
+const textApellido = document.getElementById('textApellido')
+const email = document.getElementById('email')
+const textTelefono = document.getElementById('textTelefono')
+const mensaje = document.getElementById('mensaje')
+const boton = document.getElementById('boton')
+
+formularioContacto.addEventListener('submit', (e) => {
+    e.preventDefault()
+    e.stopImmediatePropagation()
+
+    const data = new FormData(formularioContacto)
+
+    console.log(data.get('nombre'))
+    console.log(data.get('apellido'))
+    console.log(data.get('email'))
+    console.log(data.get('telefono'))
+    console.log(data.get('texto'))
+})
+
